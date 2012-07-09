18124c0cc4991e0f9c2f59f53925cbcf1357e14b
philippkueng <git@philippkueng.ch>
2012-07-09 22:31:23 +0200

    added nubnub package

--- app.js
+++ app.js
@@ -6,7 +6,32 @@ app.get('/', function(request, response) {
   response.send('Hello World from eatingstats!');
 });
 
+app.get('/pubsub', function(request, response) {
+  response.send('pubsub route');
+  console.log('got a message from the hub');
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+// subscribe to the remote hub
+
+var nubnub = require('nubnub');
+var Client = nubnub.Client;
+var cli = Client.build({
+	hub: "http://tumblr.superfeedr.com",
+	topic: "http://everrouter.tumblr.com",
+	callback: "http://eatingstats.herokuapp.com/pubsub"
+});
+
+console.log("subscribing....");
+cli.subscribe(function(err, resp, body){
+	if(err){
+		console.log(err);
+	} else {
+		console.log(resp.statusCode + ': ' + body);
+	}
+});
+
