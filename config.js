5c6b5f0d0326bde02a84449a6c6021be927b5396
philippkueng <git@philippkueng.ch>
2012-07-22 00:53:47 +0200

    added knox an S3 library to package.json; added AWS configuration details to config.js

--- config.js
+++ config.js
@@ -5,5 +5,10 @@ module.exports = {
     database: process.env.MYSQL_DATABASE ? process.env.MYSQL_DATABASE : 'eatingstats',
     port: process.env.MYSQL_PORT ? process.env.MYSQL_PORT : 3306,
     host: process.env.MYSQL_HOST ? process.env.MYSQL_HOST : 'localhost'
+  },
+  aws: {
+    key: process.env.AWS_KEY ? process.env.AWS_KEY : '',
+    secret: process.env.AWS_SECRET ? process.env.AWS_SECRET : '',
+    bucket: process.env.AWS_BUCKET ? process.env.AWS_BUCKET : 'eatingstats'
   }
 };
