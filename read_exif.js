809146dd275c1c161b132f0378369489fddf152e
Philipp Küng <i@philippkueng.ch>
2012-08-28 23:54:09 +0200

    added temporary working files

---
+++ read_exif.js
@@ -0,0 +1,12 @@
+var ExifImage = require('exif').ExifImage;
+
+try {
+    new ExifImage({ image : 'image.jpg' }, function (error, image) {
+        if (error)
+            console.log('Error: '+error.message);
+        else
+            console.log(image); // Do something with your data!
+    });
+} catch (error) {
+    console.log('Error: ' + error);
+}
