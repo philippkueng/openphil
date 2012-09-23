f9d16fdcb81433da1cf521559fc4364156b34eca
philippkueng <git@philippkueng.ch>
2012-09-23 11:39:40 +0200

    cleaned up

--- imagemagick_exif_sample.js
+++
@@ -1,6 +0,0 @@
-var im = require('imagemagick');
-im.readMetadata('image.jpg', function(err, metadata){
-  if (err) throw err;
-  console.log(metadata.exif);
-  // console.log('Shot at '+metadata.exif.dateTimeOriginal);
-})
