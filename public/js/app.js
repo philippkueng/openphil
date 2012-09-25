9a3cbb89423fd05fa6faa949381296bfaa41e1c2
philippkueng <git@philippkueng.ch>
2012-09-26 01:14:03 +0200

    finished first bar visualization of weight entries - still needs cleanup

--- public/js/app.js
+++ public/js/app.js
@@ -2,17 +2,33 @@
 	$(function(){
 		$.getJSON("/weight", function(weight_arr){
 			// console.log(weight_arr);
-			var data = [4, 8, 15, 16, 23, 42, 23, 42, 23, 12, 31, 12, 34, 23, 54, 21, 29];
+			var data = [4, 8, 15, 16, 23, 42.2, 23, 42, 23.5, 12, 31, 12, 34, 23, 54, 21, 29];
 
 			var width = 780;
 			var height = 220;
 
-			var x = d3.scale.ordinal()
-				.domain(data)
-				.rangeBands([0, width]);
+			var bar_width = (width / weight_arr.length);
+
+			// var x = d3.scale.ordinal()
+			// 	// .domain(weight_arr)
+			// 	// .domain([0, d3.max(weight_arr, function(entry){
+			// 	// 	return parseFloat(entry.weight);
+			// 	// })])
+			// 	.domain([0, d3.max(data)])
+			// 	// .domain(data)
+			// 	.rangeBands([0, bar_width]);
+
+			var x = d3.scale.linear()
+				.domain([0,1])
+				.range([0, bar_width]);
+				// .domain([0, d3.max(data)])
+				// .rangeBands([0, bar_width]);
 
 			var y = d3.scale.linear()
 				.domain([0, d3.max(data)])
+				// .domain([0, d3.max(weight_arr, function(entry){
+				// 	return parseFloat(entry.weight);
+				// })])
 				.range([0, height]);
 
 			var create_graph = function(){
@@ -31,22 +47,28 @@
 					.attr("y1", height + 10)
 					.attr("y2", height + 10)
 					.attr("stroke", "#000")
-					.attr("stroke-width", 2)
+					.attr("stroke-width", 2);
 
 				// insert the bars
 				chart
 					.selectAll('rect')
-					.data(data)
+					.data(weight_arr, function(d){
+						return parseFloat(d.weight);
+					})
 					.enter()
 					.append('rect')
-					.attr('x', function(d){
-						return x(d) + 10;
+					.attr('x', function(d, i){
+						return x(i) + 10;
+						// return x(parseFloat(d.weight)) + 10;
 					})
 					.attr('y', function(d){
-						return height - y(d);
+						return height - y(parseFloat(d.weight));
 					})
-					.attr('width', x.rangeBand())
-					.attr('height', y);
+					// .attr('width', x.rangeBand())
+					.attr('width', bar_width)
+					.attr('height', function(d){
+						return y(parseFloat(d.weight));
+					});
 			};
 
 			create_graph();
